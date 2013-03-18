c1d4ef64a0b3d831d2359ea4c2f7986ee325d0fa
James Aitken <loonypandora@gmail.com>
2013-03-18 06:46:37 +0000

    Use mustache templates, and parse the markdown

--- js/app.js
+++ js/app.js
@@ -4,45 +4,60 @@
 
 $(document).ready(function(){
 
-
+    // List of posts
     $.ajax("http://api.reddit.com/r/londonfootballmeetup/new.json?jsonp=?", {
         dataType : "json",
         data     : { limit: 5 },
         success  : function(response, textStatus, jqXHR) {
+            var $template = $("#latest-posts-template").html();
+
             $.each(response.data.children,
                 function (i, post) {
-                    var $link = $("<li/>").append(
-                        $("<a/>")
-                            .attr("href", post.data.url)
-                            .attr("title", post.data.title)
-                            .text(post.data.title)
+                    $(".latest-posts").append(
+                        Mustache.render($template, post.data)
                     );
-
-                    $(".latest-posts").append($link);
                 }
             );
         },
-        error : function() {
-            console.log(arguments);
+        error : function(jqXHR, textStatus) {
+            var $template = $("#latest-posts-template").html();
+
+            $(".latest-posts").append(
+                Mustache.render($template, {
+                    "title" : "Reddit is down :(",
+                    "url" : "#"
+                })
+            );
         }
     });
 
-
+    // Generic "About Us" Info
     $.ajax("http://api.reddit.com/r/londonfootballmeetup/about.json?jsonp=?", {
         dataType : "json",
         data     : { limit: 5 },
         success  : function(response, textStatus, jqXHR) {
-            
-           $(".about-text").text( response.data.public_description );
-            
+            $(".about-text").text( response.data.public_description );
         },
         error : function() {
             console.log(arguments);
         }
     });
 
+    // FAQ page
+    $.ajax("http://api.reddit.com/comments/19wn4j.json?jsonp=?", {
+        dataType : "json",
+        data     : { limit: 5 },
+        success  : function(response, textStatus, jqXHR) {
+            // Nice nesting reddit...
+            var selfText = response[0].data.children[0].data.selftext;
 
-
+            // Too simplistic to bother with a template
+            $(".faq-content").append( markdown.toHTML(selfText) );
+        },
+        error : function() {
+            console.log(arguments);
+        }
+    });
 
 
 
