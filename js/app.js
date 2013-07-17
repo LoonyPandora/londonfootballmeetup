0b02413d37178a7153e7467b4da5eb7333b175ec
James Aitken <loonypandora@gmail.com>
2013-07-17 16:25:38 +0100

    Something that's simple and not ugly as sin

--- js/app.js
+++ js/app.js
@@ -29,7 +29,6 @@ $(document).ready(function(){
         }
     });
 
-
     // FAQ page
     $.ajax("http://api.reddit.com/comments/19wn4j.json?jsonp=?", {
         dataType : "json",
@@ -39,8 +38,8 @@ $(document).ready(function(){
             var selfText = response[0].data.children[0].data.selftext;
 
             // Hacky and crap regex cleanup of the markdown
-            selfText = selfText.replace(/\*\*FAQ\*\*/,"");
-            selfText = selfText.replace(/Time to update FAQ! Please post below if you see anything wrong or needs adding\./,"");
+            selfText = selfText.replace(/\*\*FAQ\*\*/, "");
+            selfText = selfText.replace(/&amp;/g, "&");
 
             // Too simplistic to bother with a template
             $(".faq-content").append( markdown.toHTML(selfText) );
@@ -56,92 +55,18 @@ $(document).ready(function(){
         dataType : "json",
         data     : { limit: 5 },
         success  : function(response, textStatus, jqXHR) {
-            // Bit hacky to parse out the dates with regexen, but will do for now
-            var dates = [];
-            $.each(response.data.description.split("\n"),
-                function (i, line) {
-                    if (line.match(/\d{1,2}(?:st|nd|rd|th)/)) {
-                        dates.push(line.replace(/\W+/, ""));
-                    }
-                }
-            );
+            var description = response.data.description;
 
-            // Someone hasn't put dates in the sidebar! Default to something sensible
-            if (!dates.length){
-                dates = ["Next Saturday 2pm", "Next Sunday 2pm"]
-            }
+            // Remove links to the FAQ which we show inline
+            description = description.replace(/&amp;/g, "&");
+            description = description.replace(/New to our subreddit.+\n\n/g, "");
 
-            // Too simplistic to bother with a template
-            $(".sidebar-content").text( dates.join(" & ") );
+            $(".sidebar-content").append( markdown.toHTML(description) );
         },
         error : function() {
             console.log(arguments);
         }
     });
-
-
-    // Workaround for google maps in an iframe on bootstrap tabs / modals
-    $("a[href=#stratford]").one('click', function(event) {
-        // Get the map link from the HTML, and re-insert it into the DOM
-        $("#stratford").html(
-            $("#stratford").html()
-        );
-    });
-
-
-    // Render the hexagons using mustache
-    var $hexTemplate = $("#hexagon-template").html();
-    var i = 0;
-    $.each(people, function (key, value) {
-        $(".hexagons").append(
-            Mustache.render($hexTemplate, { "picture" : key, "name" : value })
-        );
-
-        var breakPoints = {
-            "desktopWide" : [6, 12, 19, 25, 32],
-            "desktop" : [4, 8, 13, 17, 22, 26, 31],
-            "mobileWide" : [2, 4, 7, 9, 12, 14, 17, 19, 21, 23],
-            "mobile" : [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17]
-        }
-
-        var $br = $("<br/>");
-
-        if ($.inArray(i, breakPoints.mobile) > -1) {
-            $br.addClass("mobile");
-        }
-
-        if ($.inArray(i, breakPoints.mobileWide) > -1) {
-            $br.addClass("mobile-wide");
-        }
-
-        if ($.inArray(i, breakPoints.desktop) > -1) {
-            $br.addClass("desktop");
-        }
-
-        if ($.inArray(i, breakPoints.desktopWide) > -1) {
-            $br.addClass("desktop-wide");
-        }
-
-        $(".hexagons").append($br)
-
-        i++;
-    });
-
-
-
-    // Show names on picture hover 
-    // TODO: Make it handle touch events too
-    $(".hexagons div").hover(
-        function () {
-            var $hexDiv = $(this);
-            $hexDiv.append(
-                $("<span/>").addClass("overlay").text( $hexDiv.data("name") )
-            )
-        },
-        function () {
-            $(this).find("span:last").remove();
-        }
-    );
 
 });
 
